1348a0d617058529d352d149d5c7708d63804232
origotdaskill <139981840+origotdaskill@users.noreply.github.com>
2024-10-16 20:16:29 +0100

    mongo db setup :construction:

--- server/app.js
+++ server/app.js
@@ -33,3 +33,13 @@ const storage = multer.diskStorage({
     },
 });
 const upload = multer({ storage });
+
+
+/* MONGO SETUP */     
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch(err => console.error('MongoDB connection error:', err));
+

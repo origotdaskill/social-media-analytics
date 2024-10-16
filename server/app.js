52aa8d8be327d5f2f4f31ba6a819b0716bc80be9
origotdaskill <139981840+origotdaskill@users.noreply.github.com>
2024-10-16 20:15:50 +0100

    file storage setup :construction:

--- server/app.js
+++ server/app.js
@@ -23,3 +23,13 @@ app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 dotenv.config();  
 
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage });

95e606a2a665fba045c103bbebbf984e3f486e14
origotdaskill <139981840+origotdaskill@users.noreply.github.com>
2024-10-17 12:45:39 +0100

    set add or remove friend  :construction:

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -30,3 +30,35 @@ export const getUserFriends = async (req, res) => {
 }
 };
 
+/* UPDATE */
+export const addRemoveFriend = async (req, res) => {
+    try {
+        const { id, friendId } = req.params;
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);    
+        const isFriend = user.friends.includes(friendId);
+        if (isFriend) {
+            user.friends = user.friends.filter((id) => id !== friendId);
+            friend.friends = friend.friends.filter((id) => id !== id);  
+            await user.save();
+            await friend.save();
+        } else {
+            user.friends.push(friendId);
+            friend.friends.push(id);
+            await user.save();    
+            await friend.save();
+        }
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        const formattedFriends = friends.map(       
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                return { _id, firstName, lastName, occupation, location, picturePath };
+            }
+        );
+        res.status(200).json(formattedFriends);
+    } catch (err) { 
+        res.status(500).json({ error: err.message });       
+        }   
+};
+
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ import express from "express";
 import { 
     getUser,
     getUserFriends,
+    addRemoveFriend,
 } from "../controllers/users.js";
 
 import {verifyToken} from "../middleware/auth.js";
@@ -12,5 +13,7 @@ const router = express.Router();
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
 
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router ;
